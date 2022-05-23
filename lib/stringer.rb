2b14a2da5692bcc78ba95995bda76629fd556858
rogeliojay2x1 <esperanzate.rogelio.dpc@gmail.com>
2022-05-23 13:31:38 +0800

    added methods

--- lib/stringer.rb
+++ lib/stringer.rb
@@ -1,8 +1,59 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require_relative "stringer/version"
+# require_relative "stringer/version"
 
+# module Stringer
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+require "stringer/version"
 module Stringer
-  class Error < StandardError; end
-  # Your code goes here...
-end
+  def self.spacify *strings
+    string = ""
+    strings.each_with_index do |s,i|
+      if i == 0 
+        string += s
+      else 
+        string += " " + s
+      end
+    end
+    string
+  end
+
+  def self.minify strings, max
+    length = strings.length
+    string = ""
+
+    for i in 0...length 
+      if i >= max 
+        string += "..."
+      break
+      elsif length == i
+        string += strings[i] 
+        break
+      else
+        string += strings[i]
+      end
+    end
+
+    string
+  end
+
+  def self.replacify *strings
+    string = strings[0]
+    string.split(" ").collect { |i| i == strings[1] ? strings[2] : i }.join(" ")
+  end
+
+  def self.tokenize string
+    string.split(" ")
+  end
+  
+  def self.removify *strings
+    string = strings[0]
+    string.split(" ").reject { |i| i == strings[1] }.join(" ")
+  end
+end 
+
+
+
--- spec/stringer_spec.rb
+++ spec/stringer_spec.rb
@@ -1,11 +1,39 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
+# RSpec.describe Stringer do
+#   it "has a version number" do
+#     expect(Stringer::VERSION).not_to be nil
+#   end
+
+#   it "does something useful" do
+#     expect(false).to eq(true)
+#   end
+# end
+
+require "spec_helper"
 RSpec.describe Stringer do
-  it "has a version number" do
-    expect(Stringer::VERSION).not_to be nil
+  it "concatenates undefined number of strings with a space" do 
+    expect(Stringer.spacify "Oscar", "Vazquez", "Zweig", "Olasaba", "Hernandez", "Ricardo", "Mendoza").to eq("Oscar Vazquez Zweig Olasaba Hernandez Ricardo Mendoza")
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'minify the words base on given length or return the whole word if less than given length' do
+    expect(Stringer.minify("Hello, I'm learning how to create a gem", 10)).to eq("Hello, I'm...")
   end
-end
+
+  it 'minify the words base on given length or return the whole word if less than given length' do
+    expect(Stringer.minify("Hello", 10)).to eq("Hello")
+  end
+
+  it 'Iterates over a string and replaces each instance of that word with the replacement provided.' do
+    expect(Stringer.replacify("I can't do this", "can't", "can")).to eq("I can do this")
+  end
+
+  it 'Iterates over a string and adds each word into an array, then returns that array.' do
+    expect(Stringer.tokenize("I love to code")).to eq(["I", "love", "to", "code"])
+  end
+
+  it 'Remove each instance of the second parameter within the original string' do
+    expect(Stringer.removify("I'm not a developer", "not")).to eq("I'm a developer")
+  end
+  
+end 
